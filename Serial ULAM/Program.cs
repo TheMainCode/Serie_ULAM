1b5727c417ea00ee4301418d120427de4052b94d
The Main Code <themaincodecontact@gmail.com>
2019-04-12 21:00:28 -0500

    .
    .

---
+++ Serial ULAM/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serial_ULAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Teclea un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 1)
+            {
+                if (num == 0)
+                    Console.WriteLine("Invalido");
+                else if (Math.Pow(-1, num) > 0)
+                {
+                    num = num / 2;
+                    Console.WriteLine(num + ",");
+                }
+
+                else
+                {
+                    num = (num * 3) + 1;
+                Console.WriteLine(num +",");
+            }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
